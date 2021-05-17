64aa0d04593511f1bf793549853572234c76fce6
Christopher R. Wood <chris@cyberethics.ca>
2021-05-17 18:57:55 -0400

    Add more tests

--- tests/test_dzip.py
+++ tests/test_dzip.py
@@ -4,10 +4,15 @@ import sys
 from subprocess import call
 from time import localtime, strftime
 
+try:
+    from unittest.mock import Mock
+except ImportError:
+    from mock import Mock
+
 import pytest
 
 from dzip import create_zipfile, extract_zipfile, sha256sum
-from dzip.dzip import compare_digests
+from dzip.dzip import _get_args, compare_digests, dunzip, main
 
 
 @pytest.fixture
@@ -113,3 +118,41 @@ def test_extract_dzip(basedir):
     os.chdir(basedir)
     create_zipfile("Test.zip", "Test", time=1234567890)
     extract_zipfile("Test.zip", "output", preserve_symlinks=True)
+
+
+def test__get_args_extract_kwarg_default_is_false():
+    args = _get_args(_args_list=["zipfile", "directory"])
+    assert args.extract is False
+
+
+def test__get_args_extract_kwarg_true():
+    args = _get_args(extract=True, _args_list=["test.zip", "testdir"])
+    assert args.extract is True
+
+
+def test__get_args_read_source_date_epoch_environment_variable():
+    env = os.environ
+    env["SOURCE_DATE_EPOCH"] = str(999999999)
+    args = _get_args(_args_list=["test.zip", "testdir"])
+    assert args.time == 999999999
+
+
+def test__get_args_t_flag_overrides_source_date_epoch_environement_variable():
+    env = os.environ
+    env["SOURCE_DATE_EPOCH"] = str(999999999)
+    args = _get_args(_args_list=["-t", "123454321", "test.zip", "testdir"])
+    assert args.time == 123454321
+
+
+def test_main_create(basedir):
+    os.chdir(basedir)
+    main(_args=_get_args(_args_list=["TestMainCreate.zip", "Test"]))
+    assert os.path.exists("TestMainCreate.zip") is True
+
+
+def test_dunzip_extract(monkeypatch, basedir):
+    monkeypatch.setattr("sys.exit", Mock())
+    os.chdir(basedir)
+    args = _get_args(extract=True, _args_list=["Test.zip", "DunzipDirectory"])
+    dunzip(_args=args)
+    assert os.path.exists("DunzipDirectory")
